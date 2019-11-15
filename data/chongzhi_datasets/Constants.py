44c9c82700815574e755281bd5b1c8b66a74a3f7
jacklanchantin <jacklanchantin@gmail.com>
2019-11-15 12:53:07 -0500

    remove old data

--- data/chongzhi_datasets/Constants.py
+++
@@ -1,9 +0,0 @@
-
-
-hg_file = '/bigtemp/jjl5sw/hg38/hg38.chrom_sizes'
-data_root = '/bigtemp/jjl5sw/deepENCODE/data/chongzhi_datasets/'
-cell_line='Tcell'
-chroms=['chr1','chr2','chr3','chr4','chr5','chr6','chr7','chr8','chr9','chr10','chr11','chr12','chr13','chr14','chr15','chr16','chr17','chr18','chr19','chr20','chr21','chr22','chrX','chrY']
-window_length = 200
-stride_length=200
-extended_window_length = 1000
